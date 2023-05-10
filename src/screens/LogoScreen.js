5f55dc83b4b23f8a7462e07e4d5b5ac4b4327a26
Vaithi <vaitheeshca3006@gmail.com>
2023-05-10 15:42:17 +0530

    vaithi

---
+++ src/screens/LogoScreen.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { View, Image, StyleSheet, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import g from "../assets/log4.mp4"
+
+const LogoScreen = () => {
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigation.navigate('Landing');
+    }, 3200); // 3 seconds
+  }, []);
+  return (
+    <View style={styles.container}>
+<Image source={g} />    
+</View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor:'black'
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+  title: {
+    color : 'white',
+    fontSize : 20
+  }
+});
+
+export default LogoScreen;
